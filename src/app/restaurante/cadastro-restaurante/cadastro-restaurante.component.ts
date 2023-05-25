b7d6967a9fdc34b01a668e12e125a7dda574e8ea
Miguel Moreira <miguelmoreiracr7@gmail.com>
2023-05-25 10:41:37 -0300

    finalizada a parte 1

--- src/app/restaurante/cadastro-restaurante/cadastro-restaurante.component.ts
+++ src/app/restaurante/cadastro-restaurante/cadastro-restaurante.component.ts
@@ -9,6 +9,7 @@ import { RestauranteService } from 'src/app/shared/services/restaurante.service'
   styleUrls: ['./cadastro-restaurante.component.css']
 })
 export class CadastroRestauranteComponent {
+  botaoManutencao: String;
   restauranteManutencao: Restaurante = {
     id: 0,
     nome: '',
@@ -21,13 +22,18 @@ export class CadastroRestauranteComponent {
     senha: '',
   }
   constructor(private restauranteService: RestauranteService, private activatedRoute: ActivatedRoute) {
+    this.botaoManutencao = 'Cadastrar';
     const id = this.activatedRoute.snapshot.paramMap.get('id')
     if (id) {
-      this.restauranteService.buscarPorId(Number(id)).subscribe(restaurante => this.restauranteManutencao = restaurante)
+      this.restauranteService.buscarPorId(Number(id)).subscribe(restaurante => this.restauranteManutencao = restaurante);
+      this.botaoManutencao = 'Salvar';
     }
   }
 
   inserir () {
     this.restauranteService.inserir(this.restauranteManutencao).subscribe();
+  }
+  atualizar () {
+    this.restauranteService.editar(this.restauranteManutencao).subscribe();
   }
 }
